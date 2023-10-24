7db28ec264e1b5fc1191772a89053a5e5ac768be
MarcinRozenek <marcin.rozenek@gmail.com>
2023-10-24 16:20:16 +0200

    Add project files.

---
+++ Mppd.MrDemo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mppd.MrDemo.Web.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
